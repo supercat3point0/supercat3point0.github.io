ff7285dc2294e1026fa38253c4357ea41f9380b4
Bennett Jann <bennettjann05@gmail.com>
2020-10-15 12:23:57 -0700

    Create jquery.raptorize.1.0.js

---
+++ archive/minecraft.net/store/loot/jquery.raptorize.1.0.js
@@ -0,0 +1,20 @@
+// Determine Entrance
+if(options.enterOn == 'timer') {
+    setTimeout(init, options.delayTime);
+} else if(options.enterOn == 'click') {
+    _this.bind('click', function(e) {
+        e.preventDefault();
+        if(!locked) {
+            init();
+        }
+    })
+} else if(options.enterOn == 'konami-code'){
+    var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+    $(window).bind("keydown.raptorz", function(e){
+        kkeys.push( e.keyCode );
+        if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+            init();
+            $(window).unbind('keydown.raptorz');
+        }
+    }, true);
+};
