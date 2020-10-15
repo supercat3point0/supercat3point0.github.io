0006d8fc4387e2dfdf7df6cfc40a4657f2fcc686
Bennett <69775498+supercat3point0@users.noreply.github.com>
2020-10-15 12:55:36 -0700

    Update jquery.raptorize.1.0.js

--- archive/minecraft.net/store/loot/jquery.raptorize.1.0.js
+++ archive/minecraft.net/store/loot/jquery.raptorize.1.0.js
@@ -1,20 +1,22 @@
-// Determine Entrance
-if(options.enterOn == 'timer') {
-    setTimeout(init, options.delayTime);
-} else if(options.enterOn == 'click') {
-    _this.bind('click', function(e) {
-        e.preventDefault();
-        if(!locked) {
-            init();
-        }
-    })
-} else if(options.enterOn == 'konami-code'){
-    var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
-    $(window).bind("keydown.raptorz", function(e){
-        kkeys.push( e.keyCode );
-        if ( kkeys.toString().indexOf( konami ) >= 0 ) {
-            init();
-            $(window).unbind('keydown.raptorz');
-        }
-    }, true);
+function raptorize () {
+    // Determine Entrance
+    if(options.enterOn == 'timer') {
+        setTimeout(init, options.delayTime);
+    } else if(options.enterOn == 'click') {
+        _this.bind('click', function(e) {
+            e.preventDefault();
+            if(!locked) {
+                init();
+            }
+        })
+    } else if(options.enterOn == 'konami-code'){
+        var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+        $(window).bind("keydown.raptorz", function(e){
+            kkeys.push( e.keyCode );
+            if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+                init();
+                $(window).unbind('keydown.raptorz');
+            }
+        }, true);
+    };
 };
